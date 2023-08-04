aeab2ffa08f9e8af1860725a7abf23af428a28be
Mauricio Ariza <mauricebarragan@hotmail.com>
2023-08-04 11:36:23 -0500

    first commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,9 @@ import { API_URL } from "../config";
 
 const instance = axios.create({
   baseURL: API_URL,
+  // headers:{
+  //   'Access-Control-Allow-Origin': 'http://127.0.0.1:5173'
+  // },
   withCredentials: true,
 });
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
+  process.env.MONGODB_URI || "mongodb+srv://mauritobarragan:Mauro2020@cluster0.p26dflq.mongodb.net/mernfatz?retryWrites=true&w=majority";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://127.0.0.1:5173";
