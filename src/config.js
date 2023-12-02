c8b92ccb31991c6ef1f455b5da51da4b8e2c35f9
Mauro Ariza <mauricebarragan@hotmail.com>
2023-12-02 09:02:21 -0500

    cambio en http://127.0.0.1:5173/

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://maurobarragan:Mauro2020@ac-6donq2q-shard-00-00.1kzlker.mongodb.net:27017,ac-6donq2q-shard-00-01.1kzlker.mongodb.net:27017,ac-6donq2q-shard-00-02.1kzlker.mongodb.net:27017/?ssl=true&replicaSet=atlas-u9zh2w-shard-0&authSource=admin&retryWrites=true&w=majority";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://127.0.0.1:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
