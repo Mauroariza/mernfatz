5fcef75be5cdfb6ab5a8b63fdcdd6b263ba8b5ab
Mauro Ariza <mauricebarragan@hotmail.com>
2023-12-02 21:49:11 -0500

    prueba

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -7,3 +7,30 @@ export const Input = forwardRef((props, ref) => (
     className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
   />
 ));
+
+
+/*
+
+import React, { useRef } from 'react';
+
+function TextInputWithFocusButton() {
+  // Crear una ref utilizando el hook useRef
+  const inputEl = useRef(null);
+
+  const onButtonClick = () => {
+    // Cuando el botón es clickeado, enfocar el campo de entrada utilizando la ref
+    inputEl.current.focus();
+  };
+
+  return (
+    <>
+   Asignar la ref al campo de entrada 
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
+    </>
+  );
+}
+
+export default TextInputWithFocusButton;
+
+*/
